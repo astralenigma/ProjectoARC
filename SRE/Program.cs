254fa4ab6e0017f88cceece1acacf1da73701aa1
astralenigma <astralenigma@hotmail.com>
2015-04-25 18:00:38 +0100

    Adiciona uma lista de "BIs" e adicionado o código de leitura de ficheiros.

--- SRE/Program.cs
+++ SRE/Program.cs
@@ -10,6 +10,25 @@ namespace SRE
     {
         static void Main(string[] args)
         {
+            //foreach (String eleitor in leitorEleitores())
+            //{
+            //    System.Console.WriteLine(eleitor);
+            //}
+            //Console.ReadLine();
+        }
+
+        static List<String> leitorEleitores()
+        {
+            List<String> lista=new List<String>();
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Rui\Documents\Visual Studio 2013\Projects\ProjectoARC\SRE\lista.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                lista.Add(line);
+            }
+            file.Close();
+            return lista;
         }
     }
 }
