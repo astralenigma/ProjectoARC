847985141105fec60ad1341c64cec89c42de545f
astralenigma <astralenigma@hotmail.com>
2015-04-25 20:03:00 +0100

    Criados os contadores e incrementadores dos partidos votos em branco Resolves #3, criado método de testes

--- SCV/Program.cs
+++ SCV/Program.cs
@@ -8,8 +8,43 @@ namespace SCV
 {
     class Program
     {
+        static int nmrPartidos;
+        static int nmrVotosBrancos;
+        static int[] contagemVotos;
+
         static void Main(string[] args)
         {
+            inicializacao();
         }
+
+        //Método de incrementação de votos em branco.
+        static void incrementarVotoBranco()
+        {
+            nmrVotosBrancos++;
+        }
+
+        //Método de inicialização das variáveis do servidor.
+        static void inicializacao()
+        {
+            nmrPartidos = 10;//no futuro talvez receba o número de partidos na linha de comandos
+            nmrVotosBrancos = 0;
+            contagemVotos = new int[nmrPartidos];
+            for (int i = 0; i < nmrPartidos; i++)
+            {
+                contagemVotos[i] = 0;
+            }
+        }
+
+        //Método de incrementação de votos de partido
+        static void incrementarVotoPartido(int partido)
+        {
+            contagemVotos[partido]++;
+        }
+
+        //Método de testes da praxe.
+        static void teste() { 
+
+        }
+
     }
 }
