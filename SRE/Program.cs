ce46a49d724cbf04ed2906b0ca659c7f3a7b162b
astralenigma <astralenigma@hotmail.com>
2015-04-25 19:31:54 +0100

    Acrescentados comentários da função dos métodos, acrescentado um método de verificação de BI, um método de escrita de ficheiro e um método de testes, alterado o método de leitura de ficheiros a lista de eleitores é agora uma variável global.

--- SRE/Program.cs
+++ SRE/Program.cs
@@ -8,27 +8,83 @@ namespace SRE
 {
     class Program
     {
+        private static List<String> lista;
         static void Main(string[] args)
         {
-            //foreach (String eleitor in leitorEleitores())
-            //{
-            //    System.Console.WriteLine(eleitor);
-            //}
-            //Console.ReadLine();
+            teste();
         }
 
-        static List<String> leitorEleitores()
-        {
-            List<String> lista=new List<String>();
-            string line;
 
+        //Método que Lê o ficheiro de texto.
+        static void leitorEleitores()
+        {
+            string line;
+            lista=new List<String>();
             System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Rui\Documents\Visual Studio 2013\Projects\ProjectoARC\SRE\lista.txt");
             while ((line = file.ReadLine()) != null)
             {
                 lista.Add(line);
             }
             file.Close();
-            return lista;
+        }
+
+        //Metodo que altera o ficheiro de texto.
+        static void escritorEleitores(String votador)
+        {
+            String texto="";
+            foreach (String eleitor in lista)
+            {
+                String[] separador = eleitor.Split(' ');
+
+                texto += separador[0]+" ";
+
+                if (separador[0] == votador)
+                {
+                    texto += "1";
+                }
+                else {
+                    texto+= separador[1];
+                }
+                texto += "\n";
+            }
+            System.IO.File.WriteAllText(@"C:\Users\Rui\Documents\Visual Studio 2013\Projects\ProjectoARC\SRE\lista.txt", texto);
+            leitorEleitores();
+        }
+
+        //Metodo de verificacao de votacao
+        static void votando(String bi)
+        {
+            foreach (String eleitor in lista)
+            {
+                if(bi == eleitor.Split(' ')[0]){
+                    if (eleitor.Split(' ')[1] == "0")
+                    {
+                        escritorEleitores(bi);
+                    }
+                }
+            }
+        }
+
+        //Método que imprime a lista no ecrã
+        static void imprimirEleitores()
+        {
+            foreach (String eleitor in lista)
+            {
+                System.Console.WriteLine(eleitor);
+            }
+            Console.ReadLine();
+        }
+
+        //Método de testes
+        static void teste() {
+            leitorEleitores();
+            imprimirEleitores();
+            votando("1113441241");
+            //escritorEleitores("1113441241");
+            imprimirEleitores();
+            votando("1113441241");
+            //escritorEleitores("1113441241");
+            imprimirEleitores();
         }
     }
 }
