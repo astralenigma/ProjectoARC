3ac13505899f970704dca3f72eb20b2fc723d4b2
astralenigma <astralenigma@hotmail.com>
2015-04-28 20:50:36 +0100

    Added my super magic fun Class Library due to slothy reasonings.

---
+++ Biblioteca de Classes/ProcessosComunicacao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+namespace BibliotecaDeClasses
+{
+    public class ProcessosComunicacao
+    {
+        Socket socket;
+        
+       public ProcessosComunicacao(Socket socket)
+        {
+            this.socket = socket;
+        }
+        public void enviarMensagens()
+        {
+            string mensagem = "";
+            do
+            {
+                mensagem = enviarMensagem();
+            } while (mensagem != "exit");
+
+        }
+
+        public String enviarMensagem()
+        {
+            string mensagem = "";
+            mensagem = Console.ReadLine()+"\n";
+            byte[] data = new byte[1024];
+            data = Encoding.ASCII.GetBytes(mensagem);
+            socket.Send(data);
+            return mensagem;
+        }
+
+        public void receberMensagens()
+        {
+            do
+            {
+                receberMensagem();
+            } while (socket.Connected);
+        }
+
+        public void receberMensagem()
+        {
+            byte[] data = new byte[1024];
+            socket.Receive(data);
+            string mensagemRecebida = Encoding.ASCII.GetString(data);
+            mensagemRecebida=mensagemRecebida.Replace("\0", "");
+            Console.Write(mensagemRecebida);
+        }
+    }
+}
