28c55e01c8d635d8242e96c79dda47c059f6ee59
astralenigma <astralenigma@hotmail.com>
2015-04-29 19:55:08 +0100

    Acrescentada a biblioteca de classes e métodos de conecção.

--- ProjectoARC/Program.cs
+++ ProjectoARC/Program.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BibliotecaDeClasses;
 
 namespace ProjectoARC
 {
     static class Program
     {
+        const int PORTA = 6000;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Socket socket = conectar("127.0.0.1");//acrescentar entrada de comando?
+            ProcessosComunicacao oPC = new ProcessosComunicacao(socket);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+
+        static Socket conectar(String ipStr)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipad = IPAddress.Parse(ipStr);
+            IPEndPoint ip = new IPEndPoint(ipad, PORTA);
+
+            socket.Connect(ip);
+
+            return socket;
+
         }
     }
 }
