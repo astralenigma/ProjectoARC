09f0f4a041223314a615d34ae4b5d6c3f09bbf71
astralenigma <astralenigma@hotmail.com>
2015-05-09 16:36:36 +0100

    Movido método de ligação para a janela. Praticamente todo o trabalho que tive no TRV foi refeito. Métodos mágicos aplicados.

--- ProjectoARC/Form1.cs
+++ ProjectoARC/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +13,9 @@ namespace ProjectoARC
 {
     public partial class Form1 : Form
     {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +25,15 @@ namespace ProjectoARC
         private void button1_Click(object sender, EventArgs e)
         {
             //Código onde recebe o voto escolhido.
+            NetworkStream serverStream = clientSocket.GetStream();
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes("Message from Client$");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            byte[] inStream = new byte[1025];
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            
         }
         /*Código onde os partidos são adicionados à interface.*/
         private void inicializacaoDosPartidos()
@@ -31,6 +44,12 @@ namespace ProjectoARC
                 comboBox1.Items.Add(i);   
             }
             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clientSocket.Connect("127.0.0.1", 8888);
+
         }
     }
 }
--- ProjectoARC/Program.cs
+++ ProjectoARC/Program.cs
@@ -1,42 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using BibliotecaDeClasses;
 
 namespace ProjectoARC
 {
     static class Program
     {
-        const int PORTA = 8888;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Socket socket = conectar("127.0.0.1");//acrescentar entrada de comando?
-            ProcessosComunicacao oPC = new ProcessosComunicacao(socket);//Isto é o melhor local para acrescentar estes métodos?
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        }
-
-        //Método conecção
-        static Socket conectar(String ipStr)
-        {
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPAddress ipad = IPAddress.Parse(ipStr);
-            IPEndPoint ip = new IPEndPoint(ipad, PORTA);
-
-            socket.Connect(ip);
-
-            return socket;
-
         }
     }
 }
