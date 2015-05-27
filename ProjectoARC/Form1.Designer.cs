b87081a213e73e3ff72209f607704d21cc081919
Rui Neves Carvalho <astralenigma@hotmail.com>
2015-05-28 00:22:48 +0100

    Melhoria no posicionamento da mensagem do resultado.

--- ProjectoARC/Form1.Designer.cs
+++ ProjectoARC/Form1.Designer.cs
@@ -83,7 +83,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(205, 78);
+            this.label3.Location = new System.Drawing.Point(137, 78);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 6;
