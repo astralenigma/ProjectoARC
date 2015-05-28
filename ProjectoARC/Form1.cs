b8e08c9914cfababea8eab56911807a8b11267b6
Rui Neves Carvalho <astralenigma@hotmail.com>
2015-05-28 01:09:28 +0100

    Using sorting and removal just to seem like I did something.

--- ProjectoARC/Form1.cs
+++ ProjectoARC/Form1.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+﻿using BibliotecaDeClasses;
+using System;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using BibliotecaDeClasses;
 namespace ProjectoARC
 {
     public partial class Form1 : Form
